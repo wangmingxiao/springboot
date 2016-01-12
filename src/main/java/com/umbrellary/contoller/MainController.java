b429eda8e1b502f1609901eb21ba8e454c0444b6
张瑞 <rui.crater@gmail.com>
2016-01-12 22:53:22 +0800

    fix

--- src/main/java/com/umbrellary/contoller/MainController.java
+++ src/main/java/com/umbrellary/contoller/MainController.java
@@ -9,6 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
